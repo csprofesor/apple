1b82450c91884c7fd783bd895f5c60eb1686cfad
profesör <gokturksahanss@gmail.com>
2026-08-28 01:55:11 +0300

    Add HintFilmIzle extension: HintFilmIzle/src/main/kotlin/com/csprofesor/HintFilmIzlePlugin.kt

---
+++ HintFilmIzle/src/main/kotlin/com/csprofesor/HintFilmIzlePlugin.kt
@@ -0,0 +1,8 @@
+package com.csprofesor
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+@CloudstreamPlugin
+class HintFilmIzlePlugin : Plugin() {
+    override fun load(context: Context) { registerMainAPI(HintFilmIzle()) }
+}
