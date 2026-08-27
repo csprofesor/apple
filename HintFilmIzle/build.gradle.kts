fd11ec3cadba2ce10f7bdb57037811755130458f
profesör <gokturksahanss@gmail.com>
2026-08-28 01:55:05 +0300

    Add HintFilmIzle extension: HintFilmIzle/build.gradle.kts

---
+++ HintFilmIzle/build.gradle.kts
@@ -0,0 +1,9 @@
+version = 1
+cloudstream {
+    description = "HintFilmizle.com film ve dizi eklentisi"
+    authors = listOf("csprofesor")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    iconUrl = "https://www.google.com/s2/favicons?domain=hintfilmizle.com&sz=%size%"
+    language = "tr"
+}
