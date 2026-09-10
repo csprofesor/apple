2fe04020c7b1bcae3363b39305311be54bffcaa4
profesör <gokturksahanss@gmail.com>
2026-09-11 00:00:51 +0300

    Fix Gradle repository configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,19 @@
 pluginManagement {
-    repositories { google(); mavenCentral(); gradlePluginPortal() }
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
 }
+
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories { google(); mavenCentral() }
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
 }
+
 rootProject.name = "Apple"
 include(":HintFilmIzle")
